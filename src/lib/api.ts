f0a14094467e0524f23d2a2d954a8e2290f13be3
SD <harrypotter.starwars.shrish@gmail.com>
2022-10-03 17:02:06 +0530

    Api methods for teams and goals

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -16,3 +16,24 @@ export async function isTokenValid(token: any) : Promise<boolean> {
        
     return response.status === 200;
 }
+
+export interface Goal {
+    id: string,
+    prettyId: string,
+    name: string,
+    color: string,
+    description: string
+}
+
+
+export async function getTeams(token: any): Promise<string[]> {
+    const teams: string[] = [];
+    // TODO
+    return teams;
+}
+
+export async function getGoals(token: any, teamId: number): Promise<Goal[]> {
+    const goals: Goal[] = [];
+    // TODO
+    return goals;
+}
