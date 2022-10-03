f6e8300c3566a64fcaca342b3dc8c17b1a0a489b
Prateek <prateek.deshmukh19@gmail.com>
2022-10-03 21:04:37 +0530

    added teams

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -28,12 +28,51 @@ export interface Goal {
 
 export async function getTeams(token: any): Promise<string[]> {
     const teams: string[] = [];
+    let headersList = {
+        "Authorization": token
+       }
+       
+       let response = await fetch("https://api.clickup.com/api/v2/team", { 
+         method: "GET",
+         headers: headersList
+       });
+       
+       let data = JSON.parse(await response.text())
+       let i = 0;
+        for(let team: any = data.teams[i]; i < data.teams.length; i++) {
+             teams.push(team.id);
+        }
+       
     // TODO
     return teams;
 }
 
 export async function getGoals(token: any, teamId: number): Promise<Goal[]> {
     const goals: Goal[] = [];
+      
+      const teamid = teamId;
+      const resp = await fetch(
+        `https://api.clickup.com/api/v2/team/${teamid}/goal`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: token
+          }
+        }
+      );
+      
+      const data = await resp.text();
+      const final: Goal = {
+        id: data.id,
+        prettyId: data.pretty_id,
+        name: data.name,
+        description: data.description,
+        color: data.color
+    };
+    let i = 0;
+        for(let goal: any = final; i < data.teams.length; i++) {
+             goals.push(goal);
+        }
     // TODO
     return goals;
 }
