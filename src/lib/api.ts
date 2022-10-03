238fd4991c1709ef4d7a87088feb6af4c91d17bf
Prateek Deshmukh <68650217+PrateekBing@users.noreply.github.com>
2022-10-03 15:48:05 +0530

    added token validation (#3)

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,20 @@
-export function isTokenValid(token: string) : boolean {
-    return true; // TODO
+import fetch from 'node-fetch';
+
+export async function isTokenValid(token: string) : Promise<boolean> {
+    const headersList = {
+        "Authorization": token
+       };
+       
+       const response = await fetch("https://api.clickup.com/api/v2/user", { 
+         method: "GET",
+         headers: headersList,
+      });
+       
+       if (response.status === 200)
+         {
+            return true;
+            }
+        else {
+            return false;
+            }
 }
